1a882bc1265bda51b4108bdb59fea6ca18c4e813
TheVoid-0 <marcodnmoura@gmail.com>
2021-09-23 03:13:19 -0300

    Add electron configuration

---
+++ electron/main.ts
@@ -0,0 +1,47 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+// verifica se foi passado o argumento para dar auto-reload
+const args: string[] = process.argv.slice(1);
+let watch: boolean = args.some(val => val === '--watch');
+
+function createWindow() {
+
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        backgroundColor: '#fff',
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    })
+
+    if (watch) {
+
+        // Habilita o auto-reload do angular na janela da aplicação electron
+        require('electron-reload')
+        mainWindow.loadURL('http://localhost:4200');
+
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '..', 'usb-interface-ng-electron/index.html'));
+    }
+
+
+    // Abre o inspecionador.
+    mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        // No macOS o comportamento padrão dos apps é recriar a janela
+        // ao clicar no ícone que fica na 'dock', caso não tenha nenhuma aberta.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
